3df809e20239de4eaf0d15731e0caa4fbb471a21
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-11 12:17:32 -0500

    added reactstrap and finished MVP

--- starwars/src/components/characterCard.js
+++ starwars/src/components/characterCard.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import {
+  Card, CardText, CardBody,
+  CardTitle
+} from 'reactstrap';
 import '../index.css';
+
+// const CharacterCard = props => {
+//   return (
+//     <div className = 'card-contain'>
+//       <h1>{props.character.name}</h1>
+//       <h2>Birth Year: {props.character.birth_year}</h2>
+//       <h2>Gender: {props.character.gender}</h2>
+//       {/* <h2>Home World: {props.character.homeworld}</h2> */}
+//     </div>
+//   )
+// }
 
 const CharacterCard = props => {
   return (
-    <div className = 'card-contain'>
-      <h1>{props.character.name}</h1>
-      <h2>Birth Year: {props.character.birth_year}</h2>
-      <h2>Gender: {props.character.gender}</h2>
-      {/* <h2>Home World: {props.character.homeworld}</h2> */}
+    <div className = 'card-contain' key = '{props.id}'>
+      <Card body inverse color="dark">
+        <CardBody>
+          <CardTitle>{props.character.name}</CardTitle>
+          <CardText>Birth Year: {props.character.birth_year}</CardText>
+          <CardText>Gender: {props.character.gender}</CardText>
+        </CardBody>
+      </Card>
+
     </div>
   )
 }
